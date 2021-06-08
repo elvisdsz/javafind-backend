bf59c1930cde4f8a446c931241a525b41f7b02d8
Elvis Dsouza <elvis.dsouza666@gmail.com>
2021-06-08 13:07:06 +0530

    Handle cross origin requests for dev testing

--- src/main/java/org/elvisdsouza/javafind/controller/SearchController.java
+++ src/main/java/org/elvisdsouza/javafind/controller/SearchController.java
@@ -3,6 +3,7 @@ package org.elvisdsouza.javafind.controller;
 import org.elvisdsouza.javafind.domain.SearchResult;
 import org.elvisdsouza.javafind.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin // Todo: Remove. Test Only
 @RestController
 public class SearchController {
 
